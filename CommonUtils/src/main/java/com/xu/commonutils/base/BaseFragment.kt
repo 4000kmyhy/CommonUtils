3d83d43f9bb42aca1d685cfc0020d224ded4e8ec
xujj <xujj@coocent.net>
2022-09-15 14:57:13 +0800

    加入fragment动画

--- CommonUtils/src/main/java/com/xu/commonutils/base/BaseFragment.kt
+++ CommonUtils/src/main/java/com/xu/commonutils/base/BaseFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.AnimRes
+import androidx.annotation.AnimatorRes
 import androidx.annotation.IdRes
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import com.xu.commonutils.R
 
 /**
  * desc:
@@ -17,6 +20,38 @@ import androidx.fragment.app.FragmentActivity
  **/
 abstract class BaseFragment : Fragment(), OnViewClickListener {
 
+    var mEnterAnim = 0
+    var mExitAnim = 0
+    var mPopEnterAnim = 0
+    var mPopExitAnim = 0
+
+    fun withAnimations(
+        @AnimatorRes @AnimRes enter: Int,
+        @AnimatorRes @AnimRes exit: Int,
+        @AnimatorRes @AnimRes popEnter: Int,
+        @AnimatorRes @AnimRes popExit: Int
+    ): BaseFragment {
+        mEnterAnim = enter
+        mExitAnim = exit
+        mPopEnterAnim = popEnter
+        mPopExitAnim = popExit
+        return this
+    }
+
+    fun withTranslateAnimations(): BaseFragment {
+        return withAnimations(
+            R.anim.fragment_slide_right_enter, R.anim.fragment_slide_left_exit,
+            R.anim.fragment_slide_left_enter, R.anim.fragment_slide_right_exit
+        )
+    }
+
+    fun withScaleAnimations(): BaseFragment {
+        return withAnimations(
+            R.anim.fragment_scale_enter, R.anim.fragment_scale_exit,
+            R.anim.fragment_scale_enter, R.anim.fragment_scale_exit
+        )
+    }
+
     fun navigateTo(
         activity: FragmentActivity?,
         @IdRes containerViewId: Int,
@@ -24,6 +59,7 @@ abstract class BaseFragment : Fragment(), OnViewClickListener {
     ) {
         if (activity == null) return
         activity.supportFragmentManager.beginTransaction()
+            .setCustomAnimations(mEnterAnim, mExitAnim, mPopEnterAnim, mPopExitAnim)
             .add(containerViewId, this, tag)
             .addToBackStack(null)
             .commitAllowingStateLoss()
@@ -36,6 +72,7 @@ abstract class BaseFragment : Fragment(), OnViewClickListener {
     ) {
         if (fragment == null) return
         fragment.childFragmentManager.beginTransaction()
+            .setCustomAnimations(mEnterAnim, mExitAnim, mPopEnterAnim, mPopExitAnim)
             .add(containerViewId, this, tag)
             .addToBackStack(null)
             .commitAllowingStateLoss()
